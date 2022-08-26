23fe3f21f2416dd11660e7a63695f63b1d594d50
Brad Traversy <muxlisaxoliyorova@gmail.com>
2022-08-26 19:09:54 +0500

    Добавьте файлы проекта.

---
+++ Photobook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookapp
+{
+    public class Photobooks
+
+    {
+        protected int numpage;
+
+
+        public int GetNumberPages()
+        {
+
+            return numpage;
+        }
+        Photobooks()
+        {
+            numpage = 16;
+        }
+        Photobooks(int numpage)
+        {
+            this.numpage = numpage;
+
+        }
+
+    }
+    public class BigPhotoBook : Photobooks
+    {
+        public BigPhotoBook()
+        {
+            numpage = 64;
+        }
+
+    }
+}
---
+++ bookapp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace bookapp
+{
+     public static void Main()
+    {
+        Photobooks pb = new Photobooks();
+        Console.WriteLine(pb.GetNumberPages());
+
+        Photobooks pb2 = new Photobooks(24);
+        Console.WriteLine(pb2.GetNumberPages());
+
+        BigPhotoBook bpb = new BigPhotoBook();
+        Console.WriteLine(bpb.GetNumberPages());
+    }
+}
